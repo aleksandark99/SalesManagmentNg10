951651c2ead9071f1307f71cf99bed9bcf0eb5e3
miljan_novisad <miljan.puletic@zesium.com>
2021-01-09 17:10:00 +0100

    order creation

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -44,4 +44,51 @@ export class OrderComponent implements OnInit {
     return isValid ? null : { 'whitespace': true };
   }
 
-}
+
+  editField: string;
+    personList: Array<any> = [
+      { id: 1, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
+      { id: 2, name: 'Guerra Cortez', age: 45, companyName: 'Insectus', country: 'USA', city: 'San Francisco' },
+      { id: 3, name: 'Guadalupe House', age: 26, companyName: 'Isotronic', country: 'Germany', city: 'Frankfurt am Main' },
+      { id: 4, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
+      { id: 5, name: 'Elisa Gallagher', age: 31, companyName: 'Portica', country: 'United Kingdom', city: 'London' },
+    ];
+
+    awaitingPersonList: Array<any> = [
+      { id: 6, name: 'George Vega', age: 28, companyName: 'Classical', country: 'Russia', city: 'Moscow' },
+      { id: 7, name: 'Mike Low', age: 22, companyName: 'Lou', country: 'USA', city: 'Los Angeles' },
+      { id: 8, name: 'John Derp', age: 36, companyName: 'Derping', country: 'USA', city: 'Chicago' },
+      { id: 9, name: 'Anastasia John', age: 21, companyName: 'Ajo', country: 'Brazil', city: 'Rio' },
+      { id: 10, name: 'John Maklowicz', age: 36, companyName: 'Mako', country: 'Poland', city: 'Bialystok' },
+    ];
+
+    updateList(id: number, property: string, event: any) {
+      const editField = event.target.textContent;
+      this.personList[id][property] = editField;
+    }
+
+    remove(id: any) {
+      this.awaitingPersonList.push(this.personList[id]);
+      this.personList.splice(id, 1);
+    }
+
+    add() {
+      if (this.awaitingPersonList.length > 0) {
+        const person = this.awaitingPersonList[0];
+        this.personList.push(person);
+        this.awaitingPersonList.splice(0, 1);
+      }
+    }
+
+    changeValue(id: number, property: string, event: any) {
+      this.editField = event.target.textContent;
+    }
+
+    public items = [{ id: 1, name: 'Mleko', unit : "l" }, {id: 2, name: 'Gvožđe' , unit : "kg"}, {id: 3, name: 'Trake za krečenje', unit : "m" }];  
+    
+    
+    // set placeholder text to DropDownList input element
+    public placeholder: string = 'Select a game';
+    public unit : string = ''
+
+}//class
