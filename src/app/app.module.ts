6443b6f51f6f2adb2c8abfd1755a687c1d79ac4c
Aleksandar Kotur <aleksa.kotur@gmail.com>
2021-01-09 08:38:51 +0100

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { ReactiveFormsModule , FormsModule} from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    ReactiveFormsModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: [
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  validatingForm: FormGroup;
+
+  ngOnInit() {
+    this.validatingForm = new FormGroup({
+      required: new FormControl(null, Validators.required)
+    });
+  }
+
+  get input() { return this.validatingForm.get('required'); }
+
+}
